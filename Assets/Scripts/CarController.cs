9e5739f4fb6161d1caf41e4290a91b70f9e28139
Oliver Forral <intrepion@gmail.com>
2015-08-01 17:14:22 -0700

    Added a rigid body and a temporary way to move forward

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class CarController : MonoBehaviour
 {
+    private Rigidbody2D myRigidbody2D;
+
     // Use this for initialization
     void Start ()
     {
-
+        this.myRigidbody2D = this.GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
     void Update ()
     {
         if (Input.GetKey ("up")) {
-            Debug.Log ("up");
+            this.myRigidbody2D.velocity += Vector2.up * 1f;
         }
     }
 }
