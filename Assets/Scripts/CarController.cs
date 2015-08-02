0342e269844d13fb85a4c76c326612815a285e17
Oliver Forral <intrepion@gmail.com>
2015-08-01 17:07:18 -0700

    Created a prefab an added an empty controller script

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey ("up")) {
+            Debug.Log ("up");
+        }
+    }
+}
